29d650801aa0d736b3f15f8092ef0b52f7607151
ishmeet8025 <work.ishmeet.26@gmail.com>
2025-11-05 11:09:57 +0530

    Create AboutScreen to display app information
    This screen provides information about the app's purpose and development.

---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("About")),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("This app allows users to explore, view, and favorite movies. Developed as part of MAD201 Assignment 3."),
+      ),
+    );
+  }
+}
