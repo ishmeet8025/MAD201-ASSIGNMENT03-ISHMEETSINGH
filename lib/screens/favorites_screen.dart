32fd10836e382689d1bf11dd00ac7a8afb128f3a
ishmeet8025 <work.ishmeet.26@gmail.com>
2025-11-05 11:08:28 +0530

    Add FavoritesScreen to show favorite movies
    Implements a screen to display user's favorite movies.

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../models/movie.dart';
+import '../widgets/movie_tile.dart';
+import 'movie_detail_screen.dart';
+
+/// Displays user's favorite movies only.
+class FavoritesScreen extends StatelessWidget {
+  final List<Movie> movies;
+  final Function(Movie) toggleFavorite;
+
+  const FavoritesScreen({super.key, required this.movies, required this.toggleFavorite});
+
+  @override
+  Widget build(BuildContext context) {
+    final favoriteMovies = movies.where((m) => m.isFavorite).toList();
+
+    if (favoriteMovies.isEmpty) {
+      return const Center(child: Text("No favorites added yet."));
+    }
+
+    return ListView.builder(
+      itemCount: favoriteMovies.length,
+      itemBuilder: (context, index) {
+        final movie = favoriteMovies[index];
+        return MovieTile(
+          movie: movie,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MovieDetailScreen(
+                  movie: movie,
+                  onFavoriteToggle: () => toggleFavorite(movie),
+                ),
+              ),
+            );
+          },
+          onFavoriteToggle: () => toggleFavorite(movie),
+        );
+      },
+    );
+  }
+}
